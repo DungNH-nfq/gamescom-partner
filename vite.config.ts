fdb8de104462e2383a3f35e9fb2e818940c32d99
Dung Nguyen Hoang <dung.nguyenhoang@nfq.asia>
2022-02-17 13:29:24 +0700

    Update config for deploy to github-page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import "dotenv/config";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  const pluginConfig = { plugins: [react()] };
+  const baseConfig = { ...pluginConfig };
+  const baseUrl = process.env.APP_BASE_URL;
+
+  if (mode === "staging") {
+    return {
+      ...baseConfig,
+      base: baseUrl,
+    };
+  }
+
+  return {
+    ...baseConfig,
+  };
 });
